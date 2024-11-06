83b9a748f3affd58150a6e604d69ec8fdb5aa8dc
19nehan_f <farheen.nehan07@gmail.com>
2024-11-06 12:36:09 +0000

    start

--- src/Library.java
+++ src/Library.java
@@ -1,21 +1,31 @@
+import java.util.ArrayList;
+
 public class Library {
-    private Book[] collection;
+    private ArrayList<Book> collection;    // ArrayList<Integer> myArray = new ArrayList<>();
     private int currentSize;
 
 
     public Library(int maxSize) {
-        collection = new Book[maxSize];
+        collection = new ArrayList<>();;
         currentSize = 0;
     }
 
 
     public void listBooks() {
         for (int i=0; i<currentSize; i++) {
-            Book currentBook = collection[i];
+            Book currentBook = collection.get(i);
             System.out.print(currentBook.title + ", ");
             System.out.print(currentBook.author + ", ");
             System.out.print(currentBook.pageCount + ", ");
             System.out.println(currentBook.hardback);
+        }
+    }
+
+    public void addBook(String title, String author,int pageCount, boolean hardback) {
+        Book book = new Book(title, author, pageCount, hardback);
+        if (currentSize < 10) {
+            currentSize++;
+            collection.add(book);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the library");
         Library myLibrary = new Library(10);
+        myLibrary.addBook("Dune", "Herbert, F", 412, false);
+        myLibrary.addBook("A Wizard of Earthsea", "Le Guin, U", 205, false);
+        myLibrary.addBook("The Very Hungry Caterpillar", "Carle, E", 22, true);
+
         myLibrary.listBooks();
+
     }
 }
 
-}
